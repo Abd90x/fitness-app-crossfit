b94abf4ed730f8c6f850a62797317d2eb5933e8a
Abdullatif Alrefahi <106773448+Abd90x@users.noreply.github.com>
2023-12-25 03:35:36 +0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,14 +6,14 @@ export default function Navbar() {
   return (
     <Stack
       direction={"row"}
-      sx={{ gap: { sm: "122px", xs: "40px" }, mt: { sm: "32px", xs: "20px" } }}
+      sx={{ gap: { sm: "100px", xs: "20px" }, mt: { sm: "32px", xs: "20px" } }}
       px={"20px"}
     >
       <Link to="/">
         <img
           src={Logo}
           alt="Logo"
-          style={{ width: "120px", height: "82px", margin: "0 20px" }}
+          style={{ width: "100px", height: "70px", margin: "0 10px" }}
         />
       </Link>
       <Stack direction="row" gap="40px" fontSize="24px" alignItems="flex-end">
